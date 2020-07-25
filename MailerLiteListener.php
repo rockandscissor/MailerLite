48657d6ed553da8494640e5727f6803d0ac6f55b
Craig Bowler <craig@rockandscissor.com>
2020-07-25 12:13:37 +0100

    Move field mapping to separate function, include instantiated $subscriber_data variable, include validation to only name split if one field is mapped to it

--- MailerLiteListener.php
+++ MailerLiteListener.php
@@ -18,6 +18,16 @@ class MailerLiteListener extends Listener
         'Form.submission.creating' => 'formSubmissionCreating',
     ];
 
+    public function __construct()
+    {
+        $this->subscriber_data = [
+            'fields' => [
+                'name' => [],
+            ],
+            'email' => false,
+        ];
+    }
+
     /**
      * Form Submission
      *
@@ -99,48 +109,37 @@ class MailerLiteListener extends Listener
      */
     private function addSubscriber($config, $submission)
     {
+        // Store the submission data in an array for easy reference later on
+        $submission_data = $submission->get(Arr::get($config, false));
+
         // Connect to MailerLite
         $mailerlite = new MailerLite($this->getConfig('mailerlite_api_key'));
 
-        // Set data for name and email fields
-        $subscriber_data = [
-            'fields' => [
-                'name' => $submission->get(Arr::get($config, 'name_field')),
-            ],
-            'email' => $submission->get(Arr::get($config, 'email_field')),
-        ];
+        // Set data email field
+        $this->subscriber_data['email'] = $submission->get($config['email_field'][0]);
+
+        if (!empty($config['name_field'])) { // Check if name_field is set
+            $this->doMapFields('name', $config['name_field'], $submission_data, ' ');
+        }
 
         // Check if Automatic Name Split is configured
         if ($auto_split_name = Arr::get($config, 'auto_split_name', true)) {
+            if (count($config['name_field']) === 1) { // If we don't have more than 1 mapped field to the name name field
+                // Split name by first space character
+                $name_array = explode(' ', $this->subscriber_data['fields']['name'], 2);
 
-            // Split name by first space character
-            $name_array = explode(' ', $subscriber_data['fields']['name'], 2);
-
-            // Set data
-            $subscriber_data['fields']['name'] = $name_array[0];
-            $subscriber_data['fields']['last_name'] = $name_array[1] ?? '';
+                // Set data
+                $this->subscriber_data['fields']['name'] = $name_array[0];
+                $this->subscriber_data['fields']['last_name'] = $name_array[1] ?? '';
+            }
         }
 
         // Check for mapped fields
         if ($mapped_fields = Arr::get($config, 'mapped_fields')) {
-
             // Loop through mapped fields
-            collect($mapped_fields)->map(function ($item, $key) use ($submission, $config, &$subscriber_data) {
-
-                // Store the submission data in an array for easy reference later on
-                $submission_data = $submission->get(Arr::get($config, false));
-
+            collect($mapped_fields)->map(function ($item, $key) use ($submission_data) {
                 if (!empty($item["mapped_form_fields"])) { // In case there is no mapped form field
-
-                    // Loop through each mapped form field for the given item and store in an array in prep for imploding
-                    foreach ($item["mapped_form_fields"] as $form_field) {
-                        if (array_key_exists($form_field, $submission_data)) { // Check if the array key exists
-                            $array[] = $submission_data[$form_field];
-                        }
-                    }
-
-                    // Add named subscriber field index to the MailerLite payload using a pointed reference to $subscriber_data
-                    $subscriber_data['fields'][$item['subscriber_field']] = implode(", ", $array);
+                    $this->doMapFields($item['subscriber_field'], $item["mapped_form_fields"], $submission_data);
                 }
             });
         }
@@ -154,13 +153,12 @@ class MailerLiteListener extends Listener
         if (isset($config['subscriber_group'])) {
 
             // Use the MailerLite Groups API to add the subscriber to a group
-            $response = $mailerlite->groups()->addSubscriber($config['subscriber_group'], $subscriber_data, $subscriber_options);
+            $response = $mailerlite->groups()->addSubscriber($config['subscriber_group'], $this->subscriber_data, $subscriber_options);
         } else {
 
             // Use the MailerLite Subscriber API to add the subscriber
-            $response = $mailerlite->subscribers()->create($subscriber_data, $subscriber_options);
+            $response = $mailerlite->subscribers()->create($this->subscriber_data, $subscriber_options);
         }
-
 
 
         // Check response for errors
@@ -192,5 +190,26 @@ class MailerLiteListener extends Listener
         return collect($this->getConfig('forms'))->first(function ($ignored, $data) use ($formset_name) {
             return $formset_name == Arr::get($data, 'form');
         });
+    }
+
+    /**
+     * Map the fields ready for paylod sent to MailerLite
+     *
+     * @param $fieldName string
+     * @param $fieldArr array
+     * @param $submission_data array
+     * @param $separator string
+     *
+     */
+    private function doMapFields($fieldName, $fieldArr, $submission_data, $separator = ", ")
+    {
+        foreach ($fieldArr as $field) {
+            if (array_key_exists($field, $submission_data)) { // Check if the array key exists
+                $field_data[] = $submission_data[$field];
+            }
+        };
+
+        $field_data = implode($separator, $field_data);
+        $this->subscriber_data['fields'][$fieldName] = $field_data;
     }
 }
